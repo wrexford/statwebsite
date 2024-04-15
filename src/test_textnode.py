785acd4790c38423fd64b1fd3d85005ef9a1cf01
wrexford <nlcousin@gmail.com>
2024-04-15 06:37:59 -0400

    debugged test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,13 +1,44 @@
 import unittest
 
-from textnode import TextNode
-
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
+)
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", text_type_text,"https://www.boot.dev")
+        node2 = TextNode("This is a text node", text_type_text,"https://www.boot.dev")
+        self.assertEqual(node, node2)
+    
+    def test_eq_url_none(self):
+        node = TextNode("This is a text node", text_type_bold,None)
+        node2 = TextNode("This is a text node", text_type_bold,None)
+        self.assertEqual(node, node2)
+          
+    def test_eq_falsetext(self):
+        node = TextNode("This is text node 1", text_type_italic)
+        node2 = TextNode("This is text node 2", text_type_italic)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_falsetext_type(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node", text_type_bold)
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", text_type_bold, "https://www.boot.dev")
+        self.assertEqual("TextNode(This is a text node, bold, https://www.boot.dev)", repr(node))        
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,10 @@
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode:
     def __init__(self, text, text_type,url=None):
         self.text = text
@@ -12,5 +19,5 @@ class TextNode:
             and self.url == text2.url
         )
 
-    def __rerp__(self):
-         return str(f"TextNode({self.text},{self.text_type},{self.url})")
+    def __repr__(self):
+         return str(f"TextNode({self.text}, {self.text_type}, {self.url})")
