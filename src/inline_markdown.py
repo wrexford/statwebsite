71511280b8fb0299f5a0c7e1d0ac8a27549904ce
wrexford <nlcousin@gmail.com>
2024-04-17 07:35:00 -0400

    finished debugging inline and added markdown_block

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -10,6 +10,16 @@ from textnode import (
     text_type_image
 )
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+
 def split_nodes_delimiter(old_nodes,delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
@@ -80,13 +90,11 @@ def split_nodes_link(old_nodes):
                 continue
             old_text = old_node.text #pull text string from old node
             links = extract_markdown_links(old_text) #return list of tuples of all links in string
-            print(f'Regex link result {links}')
             if len(links) == 0: #if we found no links in string, append string and move to next
                 new_nodes.append(old_node)
                 continue
             for link in links: #if we found links, iterate through tuple list
-                new_link_list = old_text.split(f"![{link[0]}]({link[1]})", 1)  #split where string matches the regex search result
-                print(f'New link result {new_link_list}')
+                new_link_list = old_text.split(f"[{link[0]}]({link[1]})", 1)  #split where string matches the regex search result
                 if len(new_link_list) != 2: 
                     raise ValueError("Invalid markdown, link section not closed")
                 if new_link_list[0] != "": #if first half of string isnt blank append text from split
