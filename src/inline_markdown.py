160517bab21b0a323d4cc07b6ed6e6c836bb2368
wrexford <nlcousin@gmail.com>
2024-04-16 22:00:32 -0400

    inline text added but not tested

---
+++ src/inline_markdown.py
@@ -0,0 +1,28 @@
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+)
+
+def split_nodes_delimiter(old_nodes,delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+        else:
+            continue
+        split_nodes = []
+        new_string_list = old_node.text.split(delimiter) #split old_node into list(min 3, but odd since delimiter would break a string into 3 parts)
+        if len(new_string_list) % 2 == 0:
+            raise ValueError("Invalid markdown, not enough segments, delimiter not constructed")
+        for i in range(0,len(new_string_list)):
+            if new_string_list[i] == "":
+                continue
+            if i % 2 == 0: #since i starts at 0, the 2, 4 and etc repsent the 1st, 3rd, 5th, etc block of text and wouldnt have the "type"
+                split_nodes.append(TextNode(new_string_list[i], text_type_text))
+            else:
+                split_nodes.append(TextNode(new_string_list[i],text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import LEAFNode
+
 text_type_text = "text"
 text_type_bold = "bold"
 text_type_italic = "italic"
@@ -10,7 +12,6 @@ class TextNode:
         self.text = text
         self.text_type = text_type
         self.url = url
-        #print(f"TextNode({self.text}, {self.text_type}, {self.url})")
 
     def __eq__(self, text2):
         return (
@@ -21,3 +22,20 @@ class TextNode:
 
     def __repr__(self):
          return str(f"TextNode({self.text}, {self.text_type}, {self.url})")
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LEAFNode(None, text_node.text)
+    elif text_node.text_type == text_type_bold:
+        return LEAFNode("b", text_node.text)
+    elif text_node.text_type == text_type_italic:
+        return LEAFNode("i", text_node.text)
+    elif text_node.text_type == text_type_code:
+        return LEAFNode("code", text_node.text)
+    elif text_node.text_type == text_type_link:
+        return LEAFNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == text_type_image:
+        return LEAFNode("img", "", {"src": text_node.url, "alt":text_node.text})
+    else:
+        raise ValueError(f"Invalid text type: {text_node.text_type}")
+
