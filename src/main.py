6915d1e40573a79b7dd9560e037baaaa820efbed
wrexford <nlcousin@gmail.com>
2024-04-17 19:46:22 -0400

    added static folder and ignore public

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
+import os, shutil
 from textnode import TextNode
 
 def main():
     run_cases = TextNode("This is a text node", "bold", "https://www.boot.dev")
     print(run_cases.__repr__())
+
+
+def copy_directory(path_from, path_to):
+    pass
+    
+
+
 
 if __name__ == "__main__":
     main()
