b0db14f26f3fb2af2c9e92785549d0acec4b5c1f
wrexford <nlcousin@gmail.com>
2024-04-16 22:01:28 -0400

    inilne tested.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -11,7 +11,6 @@ def split_nodes_delimiter(old_nodes,delimiter, text_type):
     for old_node in old_nodes:
         if old_node.text_type != text_type_text:
             new_nodes.append(old_node)
-        else:
             continue
         split_nodes = []
         new_string_list = old_node.text.split(delimiter) #split old_node into list(min 3, but odd since delimiter would break a string into 3 parts)
